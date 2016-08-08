b8ae581ca1bc72d70c14f59413bca1d5e03ae89e
Roman <w2@live.ru>
2016-08-08 03:34:05 +0300

    fixed font issue

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,9 +2,9 @@ var webpack = require('webpack');
 var config = require('./webpack.config')
 
 config.devtool = null
-// config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-//   include: /.js$/,
-//   minimize: true
-// }))
+config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+  include: /.js$/,
+  minimize: true
+}))
 
 module.exports = config
