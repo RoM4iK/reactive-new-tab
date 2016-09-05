4125ca485e4cd08c19ead9c965be4506becf1dd6
Roman <w2@live.ru>
2016-09-05 10:58:46 +0300

    build script added, updated version

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var config = require('./webpack.config')
 
+config.output.path = './build'
 config.devtool = null
 config.plugins.push(new webpack.optimize.UglifyJsPlugin({
   include: /.js$/,
